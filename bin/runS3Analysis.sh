296849cb757a7133726908f2acf02eb2c8582d6e
Marc Noguera Julian <mnoguera@mbpro-mnoguera.local>
2021-01-25 11:54:53 +0100

    parse NF results initial commit

--- bin/runS3Analysis.sh
+++ bin/runS3Analysis.sh
@@ -20,6 +20,9 @@ s3Bucket="s3://***REMOVED***/"
 NFSamplesFile=$1
 NFOutDir=$2
 
+#### Need to source a file with credentials an
+#### AWS credentials are IAM managed
+#### NextFlow credentials 
 cd /tmp/
 
 git clone https://github.com/MicrobialGenomics/viralrecon.git
@@ -39,6 +42,61 @@ nextflow run /Users/mnoguera/Documents/Work/Development/viralrecon/main.nf --inp
  --leading 20 --trailing 20 --minlen 50 --sliding_window 5 --sliding_window_quality 20
 
 ### Manual Inspection of results
+### Note that when running on cloud, results dir needs to be on s3
+### Need to download it and select results to import into DB or csv?
+aws s3 cp --recursive ${NFOutDir}results /tmp/results
+
+
+### Variables to extract 
+# FlowCellID	
+# InstrumentID	
+# s3FastqR1
+# s3FastqR2	
+# s3BamFile	
+# s3CovFile	
+# RawDataSeqs	
+# COVIDSeqs	
+# FastaSequence	
+# PercCoverage	
+# MedianCoverage	
+# QPassBool	
+
+### Files of interest
+### BCFTools section
+### s3FastqR1, and s3FastqR2 from NFSamplesFile
+### s3BamFile results/variants/bam/*.mkD.sorted.bam || contains unmapped reads
+### RawDataSeqs samtools view results/variants/bam/*.mkD.sorted.bam | wc -l
+### COVIDSeqs samtools vied -F 4 results/variants/bam/*.mkD.sorted.bam | wc -l
+### results/variants/consensus/*masked*fa    --> consensus sequence, No degenerate positions
+### results/variants/consensus/base_qc/*base_counts.tsv   --> Sequence composition  || percN=`tail -n 1 $file | awk '{print $3}'` || PercCoverage=100-percN
+# base    freq    percI
+# A   5492    18.3863408101774
+# C   3447    11.5400066956813
+# T   5788    19.3773016404419
+# G   3728    12.480749916304
+# N   11415   38.2156009373954
+### results/variants/bam/picard_metrics/*coverage_metrics  --> coverage
+# samtools  depth results/variants/bam/*.mkD.sorted.bam | awk '{sum+=$3} END { print "Average = ",sum/NR}' > ${sample}_depthOfCoverage.txt
+### results/    --> bedfile
+### No Results for insufficient coverage
+
+### Ivar
+### results/variants/consensus/*masked*fa    --> consensus sequence
+### results/variants/consensus/base_qc/*base_counts.tsv   --> Sequence composition 
+### results/variants/bam/picard_metrics/*coverage_metrics  --> coverage
+### results/    --> bedfile
+### No Results for insufficient coverage
+
+### varscan2
+
+### Custom
+### results/    --> consensus sequence
+### results/    --> Sequence composition
+### results/    --> coverage
+### results/    --> bedfile
+
+
+
 
 ### Contamination?
 ### Coverage?
@@ -48,4 +106,4 @@ nextflow run /Users/mnoguera/Documents/Work/Development/viralrecon/main.nf --inp
 ### Scan results files to keep selected variables
 ### Download to local
 
-aws s3 cp --recursive ${NFOutDir}results /tmp/results
+
