d2c0d35fc914c5f3930e418f971c30137fc2e4cb
Marc Noguera Julian <mnoguera@mbpro-mnoguera.local>
2021-05-31 16:30:16 +0200

    changes

--- bin/fetchAndUpload.sh
+++ bin/fetchAndUpload.sh
@@ -152,9 +152,9 @@ echo $s3Location
 . $MYDIR/runS3Analysis.sh /tmp/${newProjectName}_NFSamples.csv $s3Location
 
 # ### Parse NextFlow Resucdlts
-. $MYDIR/parseNFviralrecon.sh /tmp/${newProjectName}_NFSamples.csv $s3Location
+#. $MYDIR/parseNFviralrecon.sh /tmp/${newProjectName}_NFSamples.csv $s3Location
 
-$RscriptBin $MYDIR/NFProcessResults.R ${projectString}/ $samplesFile
+#$RscriptBin $MYDIR/NFProcessResults.R ${projectString}/ $samplesFile
 
 
 ### Insert results into DB, using ViralRecon, NextClade i Pangolin.
