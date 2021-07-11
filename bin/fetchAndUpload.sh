629ad98d7ce9ec4e1b45a36c2abc039b6f8cb6c6
Ubuntu <ubuntu@ip-10-1-10-153.eu-west-1.compute.internal>
2021-07-11 19:39:24 +0000

    send email upon automation using crontab

--- bin/fetchAndUpload.sh
+++ bin/fetchAndUpload.sh
@@ -42,11 +42,11 @@ name=$1 || echo "Project name not defined"
 samplesFile=$2 || echo "Metadata file not defined, will process all samples"
 idCrossFile=$3 || echo "No ID CorssFile passed as argument, filenames will need to match library_id"
 
-echo "idCrossFile is: $idCrossFile"
-run_id=`echo $name | sed s/Covid-//`
-echo "Querying database with $run_id"
-$RscriptBin $MYDIR/samples_from_basespace_project.R -i $run_id -o /tmp/
-samplesFile=/tmp/metadata_to_fetch_run_${run_id}.csv
+#echo "idCrossFile is: $idCrossFile"
+#run_id=`echo $name | sed s/Covid-//`
+#echo "Querying database with $run_id"
+#$RscriptBin $MYDIR/samples_from_basespace_project.R -i $run_id -o /tmp/
+#samplesFile=/tmp/metadata_to_fetch_run_${run_id}.csv
 
 ### Behaviour: If Id cross File is not provided $name variable will be used throughout and needs to correspond to the basespace project
 ### Otherwise: $name variable will only be used to retrieve samples, but $newProjectName will be used for further labeling
@@ -73,12 +73,17 @@ cat /tmp/${newProjectName}_project.csv
 # fi
 echo "numProjects is: $numProjects"
 if [[ $numProjects -eq 1 ]] ; then
-    echo "Exiting"
-    exit 1 ### Need to exit in order for calling python script not continuign
+	echo "Exiting, no data available (yet) for $newProjectName"
+    #exit 1 ### Need to exit in order for calling python script not continuign
 elif [[ $numProjects -gt 2 ]]; then
     echo "Exiting, too many projects for $newProjectName"
-    exit 1
+    #exit 1
 else
+    echo "idCrossFile is: $idCrossFile"
+    run_id=`echo $name | sed s/Covid-//`
+    echo "Querying database with $run_id"
+    $RscriptBin $MYDIR/samples_from_basespace_project.R -i $run_id -o /tmp/
+    samplesFile=/tmp/metadata_to_fetch_run_${run_id}.csv
     ### Obtain the project BaseSpace ID
     projectID=`cat /tmp/${newProjectName}_project.csv | grep "${name}," | awk 'BEGIN{FS=","}{print $2}'`
     ### Create a unique string for this run
@@ -165,6 +170,7 @@ else
         echo "Finishing"
         $RscriptBin $MYDIR/nf_process_results.R -S ${projectString}/ -s true
         aws s3 cp ${s3Location}$newProjectName.csv ${s3Bucket}Runs/AggregatedData/
+	python $MYDIR/sendEmail.py mnoguera@irsicaixa.es $newProjectName finished
         #### Consider running aggregated data analysis and gisaid upload.
     fi
 fi
--- bin/runWrapper.sh
+++ bin/runWrapper.sh
@@ -2,15 +2,33 @@
 
 ### Wrapper script for automatized execution
 ### 2021-06-30 12:43:01 MNJ
-MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-echo "MYDIR is $MYDIR"
-. $MYDIR/environment.sh
 
-### This will add pending runs to /tmp/covid_projects_pending.txt
-~/miniconda3/bin/python3 $MYDIR/SQS_messages.py
+LOCKDIR=/tmp/runWrapperLock.lck
 
-### This will try to run pending projects if Illumina/basespace data is available
-for run in `cat /tmp/covid_projects_pending.txt | awk 'BEGIN{FS=","}{print $1}'`
-do
-    . $MYDIR/fetchAndUpload.sh $run
-done
+if mkdir $LOCKDIR
+then
+    # Do important, exclusive stuff
+    MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+    echo "MYDIR is $MYDIR"
+    . $MYDIR/environment.sh
+    ### This will add pending runs to /tmp/covid_projects_pending.txt
+    ~/miniconda3/bin/python3 $MYDIR/SQS_messages.py
+    ### This will try to run pending projects if Illumina/basespace data is available
+    for run in `cat /tmp/covid_projects_pending.txt | awk 'BEGIN{FS=","}{print $1}'`
+    do
+      myLog=~/logs/viralrecon_"$(date +"%Y_%m_%d_%I_%M_%p").log"
+      . $MYDIR/fetchAndUpload.sh $run | tee > $myLog
+    done
+  if rmdir $LOCKDIR
+    then
+        echo "Victory is mine"
+        echo "Releasing Lock"
+    else
+        echo "Could not remove lock dir" >&2
+  fi
+else
+    # Handle error condition
+    echo "Another instance of this script is already running"
+fi
+
+
